893214fb31ffc8282782c1c0f926657fb713abed
samulimarkus <minjonlu@gmail.com>
2021-07-23 21:26:38 +0300

    Use withdefault method with comment user relationship

--- src/Models/Comment.php
+++ src/Models/Comment.php
@@ -22,6 +22,8 @@ class Comment extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withDefault([
+            'name' => 'Unknown user',
+        ]);
     }
 }
